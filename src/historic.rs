918d1f7e02963e25e483360d539bacba2cc775aa
Christian FOUCHER <christian.foucher@gmail.com>
2017-08-31 21:47:25 +0200

    Transform while to for

--- src/historic.rs
+++ src/historic.rs
@@ -44,20 +44,16 @@ impl<T : JsonDisplay + Display + Average<T>> Historic<T> {
                 let nb_elements_to_sum = historic.limit / 2;
                 //println!("Reduction queue {} nbElementsToSum = {}\n", i, nb_elements_to_sum);
                 // Accumulate on first nb_elements_to_sum element of the historic
-                let mut j = 0;
                 let mut accumulator_data = T::empty_cumulator();
-                while j < nb_elements_to_sum {
+                for j in 0..nb_elements_to_sum {
                     match historic.circular_buffer.peek_item(j) {
                             Some(data) => {data.cumulate(&mut accumulator_data);},
                             None => panic!("Problem of implementation in historic reduce method")
                         }
-                        j += 1;
                 }
                 //remove elements used for accumulation from the historic
-                j = 0;
-                while j < nb_elements_to_sum {
+                for _ in 0..nb_elements_to_sum {
                     historic.circular_buffer.get_item();
-                    j += 1;
                 }
                 // The average_data will be add to the next historic (if historic exists, otherwise it will be lost)
                 average_data = Some(T::divide(&accumulator_data, nb_elements_to_sum));
